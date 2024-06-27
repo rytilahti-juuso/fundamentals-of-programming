c5e6f79734691bcd0ab18ca5cd5ddd5cccc9ecd9
njmett <njmett@utu.fi>
2024-06-27 15:09:20 +0300

    Empty image fix

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -56,7 +56,7 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       // Replace with your project's social card
-      image: '',
+      image: 'img/UTU_logo_EN_RGB.png',
       colorMode: {
         defaultMode: 'light',
         respectPrefersColorScheme: true,
